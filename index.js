2c0ded62a2b3b99df6ba8f86874ac9ce0c933fe2
Taylor Liu <36674564+talola612@users.noreply.github.com>
2025-10-31 16:37:23 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let homeScore = document.getElementById("home-score")
+let guestScore = document.getElementById("guest-score")
+
+function add(team, score) {
+    team.textContent= parseInt(team.textContent) + parseInt(score);
+}
+
+function reset() {
+    homeScore.textContent = 0;
+    guestScore.textContent = 0;
+}
