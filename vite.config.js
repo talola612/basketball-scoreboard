18ec8ca0d371634b30de5b9be2efdb9ade6c2188
Taylor Liu <36674564+talola612@users.noreply.github.com>
2025-10-31 17:00:56 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from "vite"
+// vite.config.js
+import { defineConfig } from "vite";
 
 export default defineConfig({
-+  build: {
-+    rollupOptions: {
-+      input: {
-+        main: "./index.html"   // Vite already adds this automatically
-+        // we only need to make sure the script is imported
-+      }
-+    }
-+  },
   plugins: []
-})
+});
